491686fe400b17fd0271d42c66822a2b18c96040
Oleg Tsarenkoff <tsarenkow@icloud.com>
2021-08-13 15:37:40 +0300

    Initial Commit

---
+++ Day or Night/Day_or_NightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day_or_NightApp.swift
+//  Day or Night
+//
+//  Created by Oleg Tsarenkoff on 13.08.21.
+//
+
+import SwiftUI
+
+@main
+struct Day_or_NightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
